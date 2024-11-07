82ce4e4a22c646436f56d238ce4a80b99ed5b2b5
Ambrus <ambrusfodor.online@gmail.com>
2024-11-07 21:37:19 +0100

    Did some clarification on the different steps with some new comments

--- Project python/Project-file.py
+++ Project python/Project-file.py
@@ -1,16 +1,16 @@
-#1. Understand folder structure. Perform operations on files in different folders & 2. Automate repeating tasks using Python “for” loops.
+#Let's start by understanding the folder structure, and performing some operations
 import os
 print (os.getcwd())
-working_dir="c:/Users/Ambrus/Documents/CEU/Coding for Economists/Coding-for-Economists-project/Project python/asia-industry"
+working_dir="c:/Users/Ambrus/Documents/CEU/Coding for Economists/Coding-for-Economists-project/Project python/asia-industry" #Change the directory to the downloaded data's folder
 os.chdir(working_dir)
+print(os.listdir())
 
-#I am only goin to use the raw data, so I can delete the files and the folder
-#files_to_delete = os.listdir("clean")
-#os.chdir("clean")
-#for i in range(len(files_to_delete)):
-    #os.remove(str(files_to_delete[i]))
-#os.chdir(working_dir)
-#os.rmdir("c:/Users/Ambrus/Documents/CEU/Coding for economists/Project/asia-industry/clean")
+#I am only goin to use the raw data, so I can delete the files
+files_to_delete = os.listdir("clean")
+os.chdir("clean")
+for i in range(len(files_to_delete)):
+    os.remove(str(files_to_delete[i]))
+os.chdir(working_dir)
 
 import pandas as pd #In order to work with data tables I am going to use the Pandas library
 
@@ -22,10 +22,10 @@ print(data.info())
 
 data["Value"]=data["Value"] / 1_000_000_000 #these are very high values so I divide them by one billion 
 
-print(data["Series"].unique())
+print(data["Series"].unique()) #All of the values of this variable have some unnecessary characters after them
 
 data["Series"] = data["Series"].replace({
-    'Industrial Production, constant US$,,,': 'Industrial Production, billions of constant US$', #i indicate here that I divided these values by one billion
+    'Industrial Production, constant US$,,,': 'Industrial Production, billions of constant US$', #I indicate here that I divided these values by one billion
     'Exchange rate, new LCU per USD extended backward, period average,,': 'Exchange rate, new LCU per USD extended backward, period average',
     'CPI Price, seas. adj.,,,' : 'CPI Price, seas. adj.',
     'CPI Price, % y-o-y, not seas. adj.,,': 'CPI Price, % y-o-y, not seas. adj.',
@@ -47,7 +47,9 @@ data["YoY Change"]=data["Value"].pct_change(fill_method=None)*100
 data=data[data['Series'] != "CPI Price, % y-o-y, not seas. adj."]
 print(data["Series"].unique())
 
-data.to_csv('output.csv', mode="w") #Let's save the cleaned data set
+os.chdir(working_dir)
+os.chdir("clean")
+data.to_csv('cleaned_data.csv', mode="w") #Let's save the cleaned data set
 
 #Let's create a list of the 5 countries with the largest industrial production
 top5_ip=data[(data["Time"] == "2017") & (data["Series"] == "Industrial Production, billions of constant US$")].nlargest(5, "Value")[["Country","Value"]]
@@ -58,9 +60,9 @@ print(data.loc[(data["Country"] == "China") & (data["Series"] == "Industrial Pro
 
 import matplotlib.pyplot as plt
 
-x=data.loc[(data["Country"] == "China") & (data["Series"] == "Industrial Production, billions of constant US$"), 'Time']
+x=data.loc[(data["Country"] == "China") & (data["Series"] == "Industrial Production, billions of constant US$"), 'Time'] #I want the Time to be on the X axis
 y1=data.loc[(data["Country"] == "China") & (data["Series"] == "Industrial Production, billions of constant US$"), 'YoY Change']
-y2=data.loc[(data["Country"] == "United States") & (data["Series"] == "Industrial Production, billions of constant US$"), 'YoY Change']
+y2=data.loc[(data["Country"] == "United States") & (data["Series"] == "Industrial Production, billions of constant US$"), 'YoY Change'] #And the industrial production of the two countries on the Y axis
 
 plt.plot(x,y1, label="China", color='red')
 plt.plot(x,y2, label="USA", color='blue')
@@ -71,7 +73,5 @@ plt.ylabel("% Growth")
 plt.grid()
 plt.show()
 
-print("Value and YoY change of Industrial production of China (in billions, constant USD)")
+print("Value and YoY change of Industrial production of China (in billions, constant USD)") #Let's make a summary statistic table of Chinas industrial production
 print(data.loc[(data["Country"] == "China") & (data["Series"] == "Industrial Production, billions of constant US$"), ["YoY Change","Value"] ].describe()) 
-
-print(data.loc[data["Country"] == "China"])
